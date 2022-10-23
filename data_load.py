a150f6fce3da7a136ef8b2a19e1fba36de01d99d
EdwardElriD <115918813+EdwardElriD@users.noreply.github.com>
2022-10-23 20:33:45 +0800

    Update data_load.py

--- data_load.py
+++ data_load.py
@@ -2,9 +2,16 @@ import torch
 import torchvision
 import os
 import pandas as pd
+import torchvision.transforms as transforms
+import numpy as np
 
 
 #########################加载数据##############################
+data_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
+])
+
 def read_data(is_train=True):
     """读取检测数据集中的图像和标签"""
     data_dir = 'detection/'
@@ -13,13 +20,29 @@ def read_data(is_train=True):
     csv_data = pd.read_csv(csv_fname)
     csv_data = csv_data.set_index('img_name')
     images, targets = [], []
+    img_expend = []
     for img_name, target in csv_data.iterrows():
+        img_expend.append(torchvision.io.read_image(
+            os.path.join(data_dir, 'sysu_train' if is_train else
+            'sysu_val', 'images', f'{img_name}')))
         images.append(torchvision.io.read_image(
             os.path.join(data_dir, 'sysu_train' if is_train else
                          'sysu_val', 'images', f'{img_name}')))
+        targets.append(list(target))
+    print(img_expend[0].shape)
+    for img_name, target in csv_data.iterrows():
+        targets.append(list(target))
+
+    for index in img_expend:
+        index = data_transform(index)
+        index = np.array(index)
+        index = index.astype(np.float32)
+        index = index.transpose(2, 0, 1)
+        index = torch.tensor(index)
+        images.append(index)
         # 这里的target包含（类别，左上角x，左上角y，右下角x，右下角y），
         # 其中所有图像都具有相同的类（索引为0）
-        targets.append(list(target))
+
     return images, torch.tensor(targets).unsqueeze(1) / 256
 
 class Dataset(torch.utils.data.Dataset):
